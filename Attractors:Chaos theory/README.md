5ebf54ae42b0eac5176c19711b995610a607ce53
Ankit <2020csb1072@iitrpr.ac.in>
2024-06-08 15:50:13 +0530

    Add field simulation

--- Attractors:Chaos theory/README.md
+++ Attractors:Chaos theory/README.md
@@ -1,9 +1,16 @@
 # Attractors : Chaos theory
 
-- [x] Recurrence plot
+- ref: http://www.3d-meier.de/tut19/Seite0.html
+
 ## Recurrence plot
+
 ![recurrence plot](./resources/modulatedSineWave.png)
 
-- [ ] learn to implement fade effect, and visualize the attractor in 3D (or any diffrential equation, really)
+## Field Simulation
 
-- ref: http://www.3d-meier.de/tut19/Seite0.html
+- inspired from [this](https://www.instagram.com/reel/C5rnOYRRP1r/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==) video, I tried to simulate the field given by the following differential equation:
+
+```
+dx/dt = cos(3*y)
+dy/dt = sin(3*x)
+```
--- Attractors:Chaos theory/main.go
+++ Attractors:Chaos theory/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"chaos/pkg"
+	"image/color"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -10,11 +12,12 @@ type Game struct {
 	attractor pkg.Attractor
 }
 
-// 0 -> WIDTH
-// |
-// v
-// HEIGHT
+// 0 -> WIDTH |
+// |   Board  |
+// v		  |
+// HEIGHT	  |
 func (g *Game) Update() error {
+	g.attractor.Update()
 	return nil
 }
 
@@ -27,9 +30,21 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 	return g.attractor.GetTotalWidth(), g.attractor.GetTotalHeight()
 }
 
+func f_xy(c complex128) float64 {
+	var x float64 = real(c)
+	var y float64 = imag(c)
+	return 50 * x * math.Cos(y/50)
+}
+func g_xy(c complex128) float64 {
+	var x float64 = real(c)
+	var y float64 = imag(c)
+	return 50 * y * math.Sin(x/50)
+}
+
 func main() {
 	var g *Game = &Game{
-		attractor: pkg.NewReccurencePlots(),
+		// attractor: pkg.NewReccurencePlots(),
+		attractor: pkg.NewFieldSimulation(600, 600, 150, 40, 40, 100, f_xy, g_xy, 2, 1, false, color.RGBA{255, 153, 0, 255}, color.RGBA{58, 28, 68, 255}),
 	}
 	ebiten.SetWindowSize(g.attractor.GetTotalWidth(), g.attractor.GetTotalHeight())
 	ebiten.SetWindowTitle("Chaos theory: Attractors")
